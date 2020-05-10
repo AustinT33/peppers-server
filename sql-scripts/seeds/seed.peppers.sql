185bdc6414a3d351c5781543d867f770a6bc3c50
austint33 <austintumlinson@gmail.com>
2020-05-10 16:42:02 -0500

    setting up repo

---
+++ sql-scripts/seeds/seed.peppers.sql
@@ -0,0 +1,11 @@
+-- https://imgur.com/5D5DTOA  hydro purple bell
+-- https://imgur.com/6e2g5Zq  coming soon img
+-- https://imgur.com/dePvzY2  trinidad scorp butch t
+
+
+INSERT INTO sweetpeppers (id, name, description, image)
+VALUES
+    (1, 'Bell Pepper', 'Naturally grown sweet Bell Peppers, yields vibrant large fruits, seeds and pods are not color specific, the colors range from red, yellow, green, orange, and some even purple! Seeds are carefully dried before storage and sealed in ziplock bags.', ('https://imgur.com/6e2g5Zq')),
+    (2, 'Purple Bell Pepper', 'Delicious, crunchy, full of antioxidants, and beautifully colored, this bell pepper grows from green to purple very quickly when mature.', ('https://imgur.com/5D5DTOA')),
+    (3, 'Hydroponic Bell Pepper', 'Hydroponically grown bell peppers using all organic fertilizers, these plants generally produce extra juicy fruit and have large leaves with lots to harvest!', ('https://imgur.com/5D5DTOA')),
+    (4, 'Sweet Banana', 'Sweet and crunchy, these long yellow peppers are great for any meal and are a personal favorite!',)
