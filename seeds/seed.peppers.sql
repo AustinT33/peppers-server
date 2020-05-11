9472735e9d73c38fb26b2f1bfc8284d7e90833f5
austint33 <austintumlinson@gmail.com>
2020-05-11 13:31:35 -0500

    fixed image issue

--- seeds/seed.peppers.sql
+++ seeds/seed.peppers.sql
@@ -1,32 +1,32 @@
--- https://imgur.com/5D5DTOA  hydro purple bell
--- https://imgur.com/6e2g5Zq  coming soon img
--- https://imgur.com/dePvzY2  trinidad scorp butch t
+-- 'https://i.imgur.com/5D5DTOA.jpg'  hydro purple bell
+-- 'https://i.imgur.com/6e2g5Zq.jpg'  coming soon img
+-- 'https://i.imgur.com/dePvzY2.jpg'  trinidad scorp butch t
 
 
 INSERT INTO sweetpeppers (name, description, image)
 VALUES
-    ('Bell Pepper', 'Naturally grown sweet Bell Peppers, yields vibrant large fruits, seeds and pods are not color specific, the colors range from red, yellow, green, orange, and some even purple! Seeds are carefully dried before storage and sealed in ziplock bags.', ('https://imgur.com/6e2g5Zq')),
-    ('Purple Bell Pepper', 'Delicious, crunchy, full of antioxidants, and beautifully colored, this bell pepper grows from green to purple very quickly when mature.', ('https://imgur.com/5D5DTOA')),
-    ('Hydroponic Bell Pepper', 'Hydroponically grown bell peppers using all organic fertilizers, these plants generally produce extra juicy fruit and have large leaves with lots to harvest!', ('https://imgur.com/5D5DTOA')),
-    ('Sweet Banana', 'Sweet and crunchy, these long yellow peppers are great for any meal and are a personal favorite!',('https://imgur.com/6e2g5Zq')),
-    ('Yellow Gypsy','Sweet with a very subtle kick, these medium sized peppers turn a pale to golden yellow when mature, plant has medium to high yield.',('https://imgur.com/6e2g5Zq')),
-    ('Large Red Cherry Sweet', 'These peppers are high yeilding with small bright red peppers that are easy to harvest and make great snacks, they resemble small cherry or grape tomatoes.',('https://imgur.com/6e2g5Zq')),
-    ('Lemon Dream', 'Sweet with explosive flavor, these bright yellow peppers make a great addition as a side or in your garden!',('https://imgur.com/6e2g5Zq'));
+    ('Bell Pepper', 'Naturally grown sweet Bell Peppers, yields vibrant large fruits, seeds and pods are not color specific, the colors range from red, yellow, green, orange, and some even purple! Seeds are carefully dried before storage and sealed in ziplock bags.', ('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Purple Bell Pepper', 'Delicious, crunchy, full of antioxidants, and beautifully colored, this bell pepper grows from green to purple very quickly when mature.', ('https://i.imgur.com/5D5DTOA.jpg')),
+    ('Hydroponic Bell Pepper', 'Hydroponically grown bell peppers using all organic fertilizers, these plants generally produce extra juicy fruit and have large leaves with lots to harvest!', ('https://i.imgur.com/5D5DTOA.jpg')),
+    ('Sweet Banana', 'Sweet and crunchy, these long yellow peppers are great for any meal and are a personal favorite!',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Yellow Gypsy','Sweet with a very subtle kick, these medium sized peppers turn a pale to golden yellow when mature, plant has medium to high yield.',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Large Red Cherry Sweet', 'These peppers are high yeilding with small bright red peppers that are easy to harvest and make great snacks, they resemble small cherry or grape tomatoes.',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Lemon Dream', 'Sweet with explosive flavor, these bright yellow peppers make a great addition as a side or in your garden!',('https://i.imgur.com/6e2g5Zq.jpg'));
 
 INSERT INTO hotpeppers (name, description, image)
 VALUES
-    ('Hatch Chili', 'Very large elongated fruits, generally dark green that will eventually mature to dark almost blood red, these fruits have a moderate punch to their crunch, has high yield on most plants.',('https://imgur.com/6e2g5Zq')),
-    ('Jalapeno', 'Classic pepper, widely used in many dishes or by itself, has a moderate heat to it, will ripen from dark green to yellow to red.',('https://imgur.com/6e2g5Zq')),
-    ('Greek Pepperoncini', 'Juicy and colorful peppers that have a medium strength kick to them, goes great with many italian or greek styled dishes or just by themselves!',('https://imgur.com/6e2g5Zq')),
-    ('Maules Red Cayenne', 'This Cayenne has quite the kick to it, going from bright green to deep red, these peppers look crazy with all sorts of curls and twists, very fun to grow!',('https://imgur.com/6e2g5Zq')),
-    ('Orange Habanero', 'Spice level on the higher end of this category, this Habanero is a classic one that will start green into yellow and mature into a deep or bright orange when ripe.',('https://imgur.com/6e2g5Zq'));
+    ('Hatch Chili', 'Very large elongated fruits, generally dark green that will eventually mature to dark almost blood red, these fruits have a moderate punch to their crunch, has high yield on most plants.',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Jalapeno', 'Classic pepper, widely used in many dishes or by itself, has a moderate heat to it, will ripen from dark green to yellow to red.',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Greek Pepperoncini', 'Juicy and colorful peppers that have a medium strength kick to them, goes great with many italian or greek styled dishes or just by themselves!',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Maules Red Cayenne', 'This Cayenne has quite the kick to it, going from bright green to deep red, these peppers look crazy with all sorts of curls and twists, very fun to grow!',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Orange Habanero', 'Spice level on the higher end of this category, this Habanero is a classic one that will start green into yellow and mature into a deep or bright orange when ripe.',('https://i.imgur.com/6e2g5Zq.jpg'));
 
 INSERT INTO crazypeppers (name, description, image)
 VALUES
-    ('Butch T. Trinidad Scorpion', 'Labeled the second hottest pepper next to the Carolina Reaper, this pepper is especially fun to grow due to the exotic characteristics of the fruit, these peppers are wicked looking with a sharp stinger coming out of the bottom, they start bright green and mature into a bright red color.',('https://imgur.com/dePvzY2')),
-    ('Caribbean Red Habanero', 'This particular Habanero is one of the hotter strains, coming from the exotic Caribbean, these peppers pack heat, they will mature into a deep dark red color when ripe.',('https://imgur.com/6e2g5Zq')),
-    ('Firecracker', 'These little peppers are delightful to grow as they are very colorful and grow straight up! Their colors range from green, yellow, red, orange, and even purple, they look amazing in the garden and have a very high yield and production rate.',('https://imgur.com/6e2g5Zq'));
+    ('Butch T. Trinidad Scorpion', 'Labeled the second hottest pepper next to the Carolina Reaper, this pepper is especially fun to grow due to the exotic characteristics of the fruit, these peppers are wicked looking with a sharp stinger coming out of the bottom, they start bright green and mature into a bright red color.',('https://i.imgur.com/dePvzY2.jpg')),
+    ('Caribbean Red Habanero', 'This particular Habanero is one of the hotter strains, coming from the exotic Caribbean, these peppers pack heat, they will mature into a deep dark red color when ripe.',('https://i.imgur.com/6e2g5Zq.jpg')),
+    ('Firecracker', 'These little peppers are delightful to grow as they are very colorful and grow straight up! Their colors range from green, yellow, red, orange, and even purple, they look amazing in the garden and have a very high yield and production rate.',('https://i.imgur.com/6e2g5Zq.jpg'));
 
 INSERT INTO hybridpeppers (name, description, image)
 VALUES
-    ('n/a', 'n/a',('https://imgur.com/6e2g5Zq'));
+    ('n/a', 'n/a',('https://i.imgur.com/6e2g5Zq.jpg'));
